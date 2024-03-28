93e0117bbb14d0cfad6da4ca652f02d0a9b52333
Himanshu-AT <aristech.official@gmail.com>
2024-03-28 12:28:29 +0530

    splashScreen

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // theme: ThemeData(useMaterial3: true, colorScheme: lightColorScheme),
-      theme: ThemeData(useMaterial3: true, colorScheme: darkColorScheme),
-      // darkTheme: ThemeData(useMaterial3: true, colorScheme: darkColorScheme),
+      theme: ThemeData(useMaterial3: true, colorScheme: lightColorScheme),
+      darkTheme: ThemeData(useMaterial3: true, colorScheme: darkColorScheme),
       home: const SplashScreen(),
     );
   }
--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,35 +1,44 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:openedu/color_schemes.g.dart';
+import 'package:openedu/home_screen.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-                'assets/images/splashScreen.jpeg'), // Replace 'assets/images/logo.png' with the path to your image
-            const SizedBox(height: 20),
-            const Text(
-              'Welcome to MyApp',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
+    return AnimatedSplashScreen(
+        splash: Column(children: [
+          Center(
+            child: SizedBox(
+              width: 300,
+              height: 300,
+              child: LottieBuilder.asset("assets/anim/splashScreen.json"),
             ),
-            const SizedBox(height: 10),
-            const Text(
-              'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-              style: TextStyle(
-                fontSize: 16,
-              ),
+          ),
+          const Spacer(),
+          Text(
+            'OpenEdu',
+            style: TextStyle(
+              fontSize: 48,
+              fontWeight: FontWeight.bold,
+              color: lightColorScheme.primary,
             ),
-          ],
-        ),
-      ),
-    );
+          ),
+          Text(
+            "learn like never before",
+            style: TextStyle(
+              fontSize: 32,
+              fontWeight: FontWeight.w600,
+              color: lightColorScheme.secondary,
+            ),
+          )
+        ]),
+        splashTransition: SplashTransition.fadeTransition,
+        duration: 4000,
+        splashIconSize: 500,
+        nextScreen: const HomeScreen());
   }
 }
